ce5a19434466662dcf1315cfed6fc52dc7cd6d5b
amrit khatri <amrit_roses@hotmail.com>
2013-03-31 13:49:16 -0400

    Set Nominated_table_names

--- app/models/m_nominated.rb
+++ app/models/m_nominated.rb
@@ -1,3 +1,5 @@
 class MNominated < ActiveRecord::Base
   attr_accessible :mid, :oid, :year
+  
+  set_table_name "m_nominated"
 end
--- app/models/p_nominated.rb
+++ app/models/p_nominated.rb
@@ -1,3 +1,5 @@
 class PNominated < ActiveRecord::Base
   attr_accessible :mid, :oid, :pid, :year
+  
+  set_table_name "p_nominated"
 end
