d0e4d77d53b34f19523ceb86b7f5f36294b06882
Dan <dcmaglasang@gmail.com>
2013-04-29 08:08:46 -0400

    Start working on UI

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,26 @@
+class InfoController < ApplicationController
+
+  def index
+    # @movies = Movie.where('id > ? AND imdb_rating > 0', 50000).find(:all, :limit => 20)
+    movie_count = Movie.where("name <> '' AND name IS NOT null").count
+    @max_pages = movie_count / 20
+    
+    @idx = params[:idx].to_i
+    @idx = @idx <= 0 ? 1 : @idx
+    @idx_prev = @idx <= 1 ? nil : @idx-1
+    @idx_next = @idx >= @max_pages ? nil : @idx+1
+    
+    if @idx <= 3 
+      @page_nums = [ 1, 2, 3, 4, 5]
+    elsif @idx >= movie_count - 2
+      @page_nums = [ @max_pages-4, @max_pages-3, @max_pages-2, @max_pages-1, @max_pages]
+    else
+      @page_nums = [ @idx - 2, @idx - 1, @idx, @idx + 1, @idx + 2]
+    end
+    
+   @sort_by = params[:sort_by] || 'name'
+    
+    @movies = Movie.movie_tables(@idx, @sort_by)
+  end
+
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,15 @@
 class Movie < ActiveRecord::Base
   attr_accessible :imdb_rating, :length, :name, :release_date
+  
+  def self.movie_tables(idx, sort_by)
+    if sort_by == 'year'
+      Movie.find_by_sql ["SELECT * FROM movies WHERE name <> '' AND name IS NOT null ORDER BY YEAR(release_date) LIMIT ? OFFSET ?", 20, (idx-1)*20]
+    else
+      Movie.find_by_sql ["SELECT * FROM movies WHERE name <> '' AND name IS NOT null ORDER BY name LIMIT ? OFFSET ?", 20, (idx-1)*20]
+    end
+  end
+  
+  def genre
+    Genre.find_by_sql ["SELECT g.* FROM genres g JOIN classifieds c ON c.gid = g.id AND c.mid = ?", self.id]
+  end
 end
