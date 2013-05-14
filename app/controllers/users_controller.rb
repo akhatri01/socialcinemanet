39aab417cfd369015f1a9176df288ed16729019a
Dan <dcmaglasang@gmail.com>
2013-05-13 22:25:15 -0400

    Finished users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -118,6 +118,24 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.limit(10).all
+    
+    @user_count = User.count
+    @max_pages = @user_count / 20
+    
+    @idx = params[:idx].to_i
+    @idx = @idx <= 0 ? 1 : @idx
+    @idx_prev = @idx <= 1 ? nil : @idx-1
+    @idx_next = @idx >= @max_pages ? nil : @idx+1
+    
+    if @idx <= 3 
+      @page_nums = [ 1, 2, 3, 4, 5]
+    elsif @idx >= @max_pages - 2
+      @page_nums = [ @max_pages-4, @max_pages-3, @max_pages-2, @max_pages-1, @max_pages]
+    else
+      @page_nums = [ @idx - 2, @idx - 1, @idx, @idx + 1, @idx + 2]
+    end
+    
+    @users = User.offset((@idx-1)*20).limit(20)
 
     respond_to do |format|
       format.html # index.html.erb
