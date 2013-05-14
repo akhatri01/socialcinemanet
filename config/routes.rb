cc217b7ea5b18412a69973d1c8676acc1d5dcc75
Dan <dcmaglasang@gmail.com>
2013-05-14 00:55:21 -0400

    Finished Oscars

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,10 @@ SocialCinemaNet::Application.routes.draw do
   
   match 'oscars' => 'oscars#index', :as => 'oscars'
   match 'oscar/:id' => 'oscars#show', :as => 'oscar_show'
+  match 'oscar/:id/m_winner/:year' => 'oscars#m_nom_winner', :as => 'm_nom_winner'
+  match 'oscar/:id/p_winner/:year' => 'oscars#p_nom_winner', :as => 'p_nom_winner'
+  match 'oscar/:id/m_nom/:year' => 'oscars#m_nom', :as => 'm_nom'
+  match 'oscar/:id/p_nom/:year' => 'oscars#p_nom', :as => 'p_nom'
   
   root :to => 'info#index'
 
